266793041f251381379dded0da8cbe9b83bca98f
Wentao <wentaowilliam@outlook.com>
2024-02-06 12:20:25 -0500

    add test1

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ function test() {
   console.log("You are testing the package!");
 }
 
-module.exports = test;
+function test1() {
+  console.log("You are doing test1!");
+}
+
+module.exports = {
+  test,
+  test1,
+};
